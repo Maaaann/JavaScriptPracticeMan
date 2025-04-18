f88e4f5c65400d99cc76a259920d0fc1dc61313b
Abdelrahman Altayeb <38187446+abdelrahmaneltayeb@users.noreply.github.com>
2025-04-19 01:46:08 +0200

    Pre

---
+++ Change_Color/script.js
@@ -0,0 +1,11 @@
+
+
+var buttonEle = document.querySelector(".btn");
+var myDivEle = document.querySelector(".mydiv");
+
+var colors = ["Black", "green" ,"blue"];
+var i = 0;
+buttonEle.onclick = function(){
+    myDivEle.style.backgroundColor = colors[i];
+    i++;
+}
