fe1a9ca247c086e7d1f5504aa22f918b367301b4
Abdelrahman Altayeb <38187446+abdelrahmaneltayeb@users.noreply.github.com>
2025-04-23 00:27:03 +0200

    setting JS

---
+++ Create_table/style.js
@@ -0,0 +1,9 @@
+
+var buttonEle = document.querySelector("button");
+var rowsEle = document.getElementById("rows");
+var columnEle = document.getElementById("columns");
+
+
+rowsValue = rowsEle.value ;
+columnValue = columnEle.value ;
+
