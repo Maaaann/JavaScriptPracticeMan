83e91296d6588b32b2e20fc36d0e1c3a66c3fd79
Abdelrahman Altayeb <38187446+abdelrahmaneltayeb@users.noreply.github.com>
2025-04-23 00:45:44 +0200

    Creating table with js

--- Create_table/style.js
+++ Create_table/style.js
@@ -1,9 +1,31 @@
 
-var buttonEle = document.querySelector("button");
+var buttonEle = document.getElementById("btn");
 var rowsEle = document.getElementById("rows");
 var columnEle = document.getElementById("columns");
 
 
-rowsValue = rowsEle.value ;
-columnValue = columnEle.value ;
+var rowsValue = rowsEle.value ;
+var columnValue = columnEle.value ;
 
+buttonEle.addEventListener("click",create_table);
+
+function create_table(){
+    
+    let body = document.body;
+    let table = document.createElement("table")  // <table></table>
+    
+
+    for(var i = 0 ; i < 2; i++){
+        let tr = document.createElement("tr")
+        table.appendChild(tr);  // <table> <tr> </tr> </table>  
+
+        for(var j = 0 ; j < 2; j++){
+            let td = document.createElement("td")
+            tr.appendChild(td);  //  <tr> <td></td> </tr>  
+        }
+    }
+
+    body.appendChild(table) // put the table element inside the body 
+}
+
+create_table();
