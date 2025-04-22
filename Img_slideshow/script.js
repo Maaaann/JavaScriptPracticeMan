cdecb46aa3994e501a53cc29943e6613d4c46c86
Abdelrahman Altayeb <38187446+abdelrahmaneltayeb@users.noreply.github.com>
2025-04-22 23:22:24 +0200

    optimization

--- Img_slideshow/script.js
+++ Img_slideshow/script.js
@@ -5,10 +5,14 @@ let img = ["img/1.jpg","img/2.jpg","img/3.jpg" ];
 let i =0;
 function sildeShow(){
     imgSlide.setAttribute("src",img[i]);
-    i++
+    
+    
     if (i >= img.length){
         i = 0;
+    } else {
+        i++;
     }
+    
     setTimeout(function(){
         sildeShow();
     },2000)
