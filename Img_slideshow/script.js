d821756006d34f8a83cb672a83d53b145dc3e0ad
Abdelrahman Altayeb <38187446+abdelrahmaneltayeb@users.noreply.github.com>
2025-04-22 23:19:36 +0200

    Done

--- Img_slideshow/script.js
+++ Img_slideshow/script.js
@@ -1,5 +1,18 @@
 
 let imgSlide = document.getElementById("image");
+let img = ["img/1.jpg","img/2.jpg","img/3.jpg" ];
 
+let i =0;
+function sildeShow(){
+    imgSlide.setAttribute("src",img[i]);
+    i++
+    if (i >= img.length){
+        i = 0;
+    }
+    setTimeout(function(){
+        sildeShow();
+    },2000)
 
-imgSlide.setAttribute("src","img/img1.jpg");
+}
+
+sildeShow()
