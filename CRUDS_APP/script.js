6af1fdd80635953162de1f71e53ed6e5e5fc6ef0
Abdelrahman Altayeb <38187446+abdelrahmaneltayeb@users.noreply.github.com>
2025-04-26 01:46:10 +0300

    Create Clear function and data storage locally

--- CRUDS_APP/script.js
+++ CRUDS_APP/script.js
@@ -10,6 +10,7 @@ let submit = document.getElementById("submit");
 
 
 //-------------------Getting the Total---------------//
+
 function get_total(){
 
     if(price.value !=""){
@@ -27,3 +28,45 @@ function get_total(){
 } //end of total function
 
 
+
+//---Local Storage Check---//
+let data_product;
+if( localStorage.data_product != null){
+    data_product = JSON.parse(localStorage.data_product);
+}else{
+    data_product = [];
+}
+
+//-------------------The Create Function---------------//
+submit.onclick = function create_product(){
+    let newProduct = {
+        title:title.value,
+        price:price.value,
+        taxes:taxes.value,
+        ads:ads.value,
+        discount:discount.value,
+        count:count.value,
+        category:category.value,
+        total:total.innerHTML,
+    }
+    // adding the data to our array
+    data_product.push(newProduct);
+    // storing the data local & converting it to strings  
+    localStorage.setItem("product",JSON.stringify(data_product));
+
+    clear_data();
+} //end of creating product function
+
+
+//-------------------Clear Inputs---------------//
+
+function clear_data(){
+    title.value = "";
+    price.value = "";
+    taxes.value = "";
+    ads.value = "";
+    discount.value = "";
+    count.value = "";
+    category.value = "";
+    total.innerHTML = "";
+};
